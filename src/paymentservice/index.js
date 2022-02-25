542fa03845a2294e2326edf045e89747b02e2d4f
Anthony Angelo <anthonyangelo@yahoo.com>
2022-02-25 18:54:24 -0500

    Update

--- src/paymentservice/index.js
+++ src/paymentservice/index.js
@@ -14,28 +14,27 @@
  * limitations under the License.
  */
 
-'use strict';
+// require('@google-cloud/profiler').start({
+//   serviceContext: {
+//     service: 'paymentservice',
+//     version: '1.0.0'
+//   }
+// });
 
-require('@google-cloud/profiler').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: '1.0.0'
-  }
-});
-require('@google-cloud/trace-agent').start();
-require('@google-cloud/debug-agent').start({
-  serviceContext: {
-    service: 'paymentservice',
-    version: 'VERSION'
-  }
-});
+// require('@google-cloud/trace-agent').start();
+// require('@google-cloud/debug-agent').start({
+//   serviceContext: {
+//     service: 'paymentservice',
+//     version: 'VERSION'
+//   }
+// });
+
+'use strict';
 
 const path = require('path');
 const HipsterShopServer = require('./server');
-
 const PORT = process.env['PORT'];
 const PROTO_PATH = path.join(__dirname, '/proto/');
-
 const server = new HipsterShopServer(PROTO_PATH, PORT);
 
 server.listen();
