51de42a62313fe1b4f9d30251044533fe1bef1ec
Anthony Angelo <anthonyangelo@yahoo.com>
2022-03-24 00:26:33 -0400

    Update

--- tests/frontend/test_frontend.py
+++ tests/frontend/test_frontend.py
@@ -1,26 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
+import pytest
 from random import randint
 from time import sleep
 
-class TestStringMethods(unittest.TestCase):
+def test_1():
+    # Test
+    sleep(randint(1,3))
+    assert True
 
-    def test_1(self):
-        # Test
-        sleep(randint(1,3))
-        self.assertTrue(True,"This should be true")
+def test_2():
+    # Test
+    sleep(randint(1,3))
+    assert True
 
-    def test_2(self):
-        # Test
-        sleep(randint(1,3))
-        self.assertTrue(True,"This should be true")
-
-    def test_3(self):
-        # Test
-        sleep(randint(1,3))
-        self.assertTrue(True,"This should be true")                
+def test_3():
+    # Test
+    sleep(randint(1,3))
+    assert True
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
