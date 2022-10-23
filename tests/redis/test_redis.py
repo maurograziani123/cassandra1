46c277cdfd658aafab219193cca8d842e4b5afd3
dennis kuipers <61538995+denniskuipersSN@users.noreply.github.com>
2022-10-23 09:32:37 +0200

    DevOPs config AB#31

---
+++ tests/redis/test_redis.py
@@ -0,0 +1,25 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+from random import randint
+from time import sleep
+
+def test_1():
+    # Test
+    sleep(randint(1,3))
+    assert True
+
+def test_2():
+    # Test
+    sleep(randint(1,3))
+    assert True
+
+def test_3():
+    # Test
+    sleep(randint(1,3))
+    assert True
+
+if __name__ == '__main__':
+    pytest.main()
